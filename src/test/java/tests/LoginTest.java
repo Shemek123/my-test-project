349fd1b0435a9bdafbfa8abfe4d6f2ffa6f4e971
namjon <przemek.nojman@gmail.com>
2025-04-28 06:41:26 +0200

    captureScreenshots

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import constants.AdminData;
+import listeners.TestListener;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
